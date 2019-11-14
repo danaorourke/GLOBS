920cb5edef40fbb08694d3a20c69dd1c256e2e0b
Dana O'Rourke <dana@cupofteacreations.com>
2019-11-13 16:30:12 -0800

    Pets save to database.

--- app/Http/Controllers/PetsController.php
+++ app/Http/Controllers/PetsController.php
@@ -35,62 +35,23 @@ class PetsController extends Controller
     $sum = 0;
     $sum = $validatedData['health'] + $validatedData['defense'] + $validatedData['attack'] + $validatedData['speed'] + $validatedData['stamina'] + $validatedData['accuracy'];
     if ($sum !== 40) {
-      return back()->withInput()->withErrors(array('Stats must equal 40.'));
-//      return redirect('/pets/adopt', ['POST' => $validatedData ])->with('error', 'Stats must equal 40.');
+      return back()->withInput()->withErrors(array('Stats total must equal 40.'));
     }
     
-  
-/*
+    // set up the pet from the response
     $pet = new Pet();
-    
-    $pet->name = $request->name;
+
+    $pet->name = $validatedData['name'];
     $pet->user_id = Auth::id();
     
-    $pet->health = $request->health;
-    $pet->defense = $request->defense;
-    $pet->attack = $request->attack;
-    $pet->speed = $request->speed;
-    $pet->stamina = $request->stamina;
-    $pet->accuracy = $request->accuracy;
+    $pet->health = $validatedData['health'];
+    $pet->defense = $validatedData['defense'];
+    $pet->attack = $validatedData['attack'];
+    $pet->speed = $validatedData['speed'];
+    $pet->stamina = $validatedData['stamina'];
+    $pet->accuracy = $validatedData['accuracy'];
     
     $pet->save();
-    
-*/
-/*
-      $pet = new Pet();
-      $pet->name = $request->slimeName;
-      $sum = 0;
-
-      // check that each stat is a number
-      if (
-        is_numeric($request->health) &&
-        is_numeric($request->defense) &&
-        is_numeric($request->attack) &&
-        is_numeric($request->speed) &&
-        is_numeric($request->stamina) &&
-        is_numeric($request->accuracy)
-      ) {
-        // add up the stats
-        $sum = (int)$request->health + (int)$request->defense + (int)$request->attack + (int)$request->speed + (int)$request->stamina + (int)$request->accuracy;
-        
-        if ($sum == 40) {
-          if ($request->health >= 5 && $request->health <= 10) { $pet->health = $request->health; }
-          if ($request->defense >= 5 && $request->health <= 10) { $pet->defense = $request->defense; }
-          if ($request->attack >= 5 && $request->health <= 10) { $pet->attack = $request->attack; }
-          if ($request->speed >= 5 && $request->health <= 10) { $pet->speed = $request->speed; }
-          if ($request->stamina >= 5 && $request->stamina <= 10) { $pet->stamina = $request->stamina; }
-          if ($request->accuracy >= 5 && $request->accuracy <= 10) { $pet->accuracy = $request->accuracy; }
-          $pet->user_id = Auth::id();
-          
-          $pet->save();
-          return redirect('/pets')->with('success', 'New Pet Adopted!');
-          // return to individual pet profile
-        } else {
-          return redirect('/pets/adopt');
-        }
-      } else {
-        return redirect('/pets/adopt');
-      }
-*/
   }
+  
 }
