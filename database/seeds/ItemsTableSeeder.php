ee620fb2ca53e3b33d8e5311b2faaf434984c979
Dana O'Rourke <dana@cupofteacreations.com>
2019-11-14 16:00:33 -0800

    Seeders added for Items and Shops, sample entry in both.

---
+++ database/seeds/ItemsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert(
+        [
+          'name' => 'GeneroLabs Slime Chow',
+          'description' => 'The most generic of foodstuffs. While not fit for human consumption, it seems to fulfill the basic needs of the average slime.'
+        ]
+      );
+    }
+}
---
+++ database/seeds/ShopsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ShopsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('shops')->insert(
+        [
+          'name' => 'Plethora Provisions',
+          'description' => 'Plethora Provisions sells all manner of foodstuffs for the discerning pet owner.'
+        ]
+      );
+    }
+}
